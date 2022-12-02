1c43cc9562034c698fe1bf4183b5dce404cf4e50
chordpli <chordplaylist@gmail.com>
2022-12-02 14:21:22 +0900

    feat: build project

---
+++ src/main/java/com/restcontroller/advice/RestcontrollerAdviceReviewApplication.java
@@ -0,0 +1,13 @@
+package com.restcontroller.advice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrollerAdviceReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestcontrollerAdviceReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restcontroller/advice/RestcontrollerAdviceReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restcontroller.advice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrollerAdviceReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
