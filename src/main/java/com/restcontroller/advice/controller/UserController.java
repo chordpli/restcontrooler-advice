b37b6e0ac50004274dcaf32f10b3749bdb11ad2c
chordpli <chordplaylist@gmail.com>
2022-12-04 23:58:11 +0900

    feat: join api, controller

--- src/main/java/com/restcontroller/advice/controller/UserController.java
+++ src/main/java/com/restcontroller/advice/controller/UserController.java
@@ -1,10 +1,26 @@
 package com.restcontroller.advice.controller;
 
+import com.restcontroller.advice.domain.Response;
+import com.restcontroller.advice.domain.User;
+import com.restcontroller.advice.domain.dto.UserJoinRequest;
+import com.restcontroller.advice.domain.dto.UserJoinResponse;
+import com.restcontroller.advice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest dto) {
+        User user = userService.join(dto);
+        return Response.success(new UserJoinResponse());
+    }
 }
