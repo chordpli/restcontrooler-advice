c0074487dd9a6779ddf892893308e922d7c263b3
chordpli <chordplaylist@gmail.com>
2022-12-05 01:07:11 +0900

    feat: create login api

--- src/main/java/com/restcontroller/advice/controller/UserController.java
+++ src/main/java/com/restcontroller/advice/controller/UserController.java
@@ -2,10 +2,11 @@ package com.restcontroller.advice.controller;
 
 import com.restcontroller.advice.domain.Response;
 import com.restcontroller.advice.domain.User;
-import com.restcontroller.advice.domain.dto.UserJoinRequest;
-import com.restcontroller.advice.domain.dto.UserJoinResponse;
+import com.restcontroller.advice.domain.dto.*;
 import com.restcontroller.advice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
+
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest dto) {
-        User user = userService.join(dto);
-        return Response.success(new UserJoinResponse());
+        log.info("join dto : {}", dto);
+        UserDto user = userService.join(dto);
+        log.info("saved User={}, mail = {}", user.getUserName(), user.getEmailAddress());
+        return Response.success(new UserJoinResponse(user.getUserName(), user.getEmailAddress()));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest dto) {
+        String token = userService.login(dto);
+        return Response.success(new UserLoginResponse(token));
     }
 }
--- src/main/java/com/restcontroller/advice/service/UserService.java
+++ src/main/java/com/restcontroller/advice/service/UserService.java
@@ -1,7 +1,47 @@
 package com.restcontroller.advice.service;
 
+import com.restcontroller.advice.domain.User;
+import com.restcontroller.advice.domain.dto.UserDto;
+import com.restcontroller.advice.domain.dto.UserJoinRequest;
+import com.restcontroller.advice.domain.dto.UserLoginRequest;
+import com.restcontroller.advice.repository.UserRepository;
+import com.restcontroller.advice.util.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    private long expireTimeMs = 1000 * 60 * 60;
+
+    public UserDto join(UserJoinRequest dto) {
+        User savedUser = userRepository.save(dto.toEntity(encoder.encode(dto.getPassword())));
+        return UserDto.builder()
+                .userName(savedUser.getUserName())
+                .emailAddress(savedUser.getEmailAddress())
+                .build();
+    }
+
+    public String login(UserLoginRequest dto) {
+        // user name 확인
+        User user = userRepository.findByUserName(dto.getUserName())
+                .orElseThrow(RuntimeException::new);
+
+        // password 일치
+        if (!encoder.matches(dto.getPassword(), user.getPassword())) {
+            throw new RuntimeException();
+        }
+
+        return JwtTokenUtil.createToken(dto.getUserName(), secretKey, expireTimeMs);
+    }
 }
