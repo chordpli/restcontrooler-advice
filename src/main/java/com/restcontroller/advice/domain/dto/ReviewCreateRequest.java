063dccd5be1b9680203ac6659a44b05033fbd87b
chordpli <chordplaylist@gmail.com>
2022-12-05 11:34:13 +0900

    feat: create api Review write

---
+++ src/main/java/com/restcontroller/advice/domain/dto/ReviewCreateRequest.java
@@ -0,0 +1,9 @@
+package com.restcontroller.advice.domain.dto;
+
+public class ReviewCreateRequest {
+
+    private int hospitalId;
+    private String title;
+    private String content;
+    private String userName;
+}
