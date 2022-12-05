45eb5a35f090ae3ef92b7c3c958c99c3aac4d254
chordpli <chordplaylist@gmail.com>
2022-12-05 15:31:32 +0900

    feat: Authentication 추가

--- src/main/java/com/restcontroller/advice/controller/ReviewController.java
+++ src/main/java/com/restcontroller/advice/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package com.restcontroller.advice.controller;
 
 import com.restcontroller.advice.domain.dto.ReviewCreateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@Slf4j
 public class ReviewController {
 
     @PostMapping
-    public String write(@RequestBody ReviewCreateRequest dto) {
+    public String write(@RequestBody ReviewCreateRequest dto, Authentication authentication) {
+        log.info("Controller user = {}", authentication.getName());
         return "리뷰 등록에 성공 했습니다.";
     }
 }
